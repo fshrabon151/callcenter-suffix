6f420f08c56855497a7dce2ecbfb7f0ecc5880a4
F Shrabon <fshrabon151@gmail.com>
2022-06-19 19:30:57 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @format */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      poppins: ["'Poppins'"],
+      sora: ["'Sora'"],
+    },
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
+    },
+    extend: {
+      colors: {
+        primary: "#1A1AFF",
+        secondary: "#7A7A7A"
+      },
+    },
+  },
+  plugins: [],
+};
