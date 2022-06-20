ceb7e759a04d7443e8ad02f32666a7a3d890bd58
F Shrabon <fshrabon151@gmail.com>
2022-06-20 14:07:23 +0600

    Brushup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-const myNav = document.getElementById("menu");
 const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu-sm");
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
-window.onscroll = () => {
-  if (window.scrollY > 150) {
-    myNav.classList.add("py-2");
-
-    myNav.classList.remove("py-4");
-  } else {
-    myNav.classList.add("py-4");
-    myNav.classList.remove("py-2");
-  }
-};
 
 $(".menu-content").on("click", "a", function () {
   $(".menu-content a.text-primary").removeClass("text-primary");
