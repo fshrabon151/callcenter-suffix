75a82ed47ab4b295351696573f9cfa4e895e5c07
F Shrabon <fshrabon151@gmail.com>
2022-06-20 14:02:30 +0600

    Active page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
       poppins: ["'Poppins'"],
       sora: ["'Sora'"],
     },
-    screens: {
-      sm: "576px",
-      md: "768px",
-      lg: "992px",
-      xl: "1200px",
-      xxl: "1400px",
-    },
+    // screens: {
+    //   sm: "576px",
+    //   md: "768px",
+    //   lg: "992px",
+    //   xl: "1200px",
+    //   xxl: "1400px",
+    // },
     extend: {
       colors: {
         primary: "#1A1AFF",
